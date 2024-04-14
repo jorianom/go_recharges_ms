7f8c296ee5f249a874a3bffb67c22bc025e715e1
Riaño John <jhon091199@gmail.com>
2024-04-14 18:34:50 -0500

    test whitout rabbit

--- main.go
+++ main.go
@@ -1,68 +1,65 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/jorianom/go-recharges-ms/routes"
-	"github.com/streadway/amqp"
 )
 
 func main() {
 	// Define los parámetros de conexión a RabbitMQ
-	rabbitMQHost := "host.docker.internal"
-	rabbitMQURL := "amqp://guest:guest@" + rabbitMQHost + ":5672/"
-	queueName := "recharges"
+	// rabbitMQHost := "host.docker.internal"
+	// rabbitMQURL := "amqp://guest:guest@" + rabbitMQHost + ":5672/"
+	// queueName := "recharges"
 
-	// Establece una conexión a RabbitMQ
-	conn, err := amqp.Dial(rabbitMQURL)
-	if err != nil {
-		log.Fatal("Error al conectar a RabbitMQ:", err)
-	}
-	defer conn.Close()
+	// // Establece una conexión a RabbitMQ
+	// conn, err := amqp.Dial(rabbitMQURL)
+	// if err != nil {
+	// 	log.Fatal("Error al conectar a RabbitMQ:", err)
+	// }
+	// defer conn.Close()
 
-	// Crea un canal
-	ch, err := conn.Channel()
-	if err != nil {
-		log.Fatal("Error al abrir un canal:", err)
-	}
-	// Declara la cola (si no existe)
-	_, err = ch.QueueDeclare(
-		queueName, // Nombre de la cola
-		true,      // Duradera
-		false,     // Eliminar cuando no se usa
-		false,     // Exclusiva
-		false,     // No esperar
-		nil,       // Argumentos
-	)
-	if err != nil {
-		log.Fatalf("Error al declarar la cola: %v", err)
-	}
-	defer ch.Close()
-	// Consume mensajes de la cola
-	msgs, err := ch.Consume(
-		queueName, // Nombre de la cola
-		"",        // Etiqueta del consumidor (dejar en blanco para una etiqueta generada automáticamente)
-		true,      // AutoAck (true para confirmar automáticamente los mensajes)
-		false,     // Exclusive (true para que solo esta conexión pueda acceder a la cola)
-		false,     // NoLocal (true para no recibir mensajes publicados por esta conexión)
-		false,     // NoWait (true para no esperar una respuesta de confirmación)
-		nil,       // Argumentos adicionales
-	)
-	if err != nil {
-		log.Fatal("Error al consumir mensajes:", err)
-	}
+	// // Crea un canal
+	// ch, err := conn.Channel()
+	// if err != nil {
+	// 	log.Fatal("Error al abrir un canal:", err)
+	// }
+	// // Declara la cola (si no existe)
+	// _, err = ch.QueueDeclare(
+	// 	queueName, // Nombre de la cola
+	// 	true,      // Duradera
+	// 	false,     // Eliminar cuando no se usa
+	// 	false,     // Exclusiva
+	// 	false,     // No esperar
+	// 	nil,       // Argumentos
+	// )
+	// if err != nil {
+	// 	log.Fatalf("Error al declarar la cola: %v", err)
+	// }
+	// defer ch.Close()
+	// // Consume mensajes de la cola
+	// msgs, err := ch.Consume(
+	// 	queueName, // Nombre de la cola
+	// 	"",        // Etiqueta del consumidor (dejar en blanco para una etiqueta generada automáticamente)
+	// 	true,      // AutoAck (true para confirmar automáticamente los mensajes)
+	// 	false,     // Exclusive (true para que solo esta conexión pueda acceder a la cola)
+	// 	false,     // NoLocal (true para no recibir mensajes publicados por esta conexión)
+	// 	false,     // NoWait (true para no esperar una respuesta de confirmación)
+	// 	nil,       // Argumentos adicionales
+	// )
+	// if err != nil {
+	// 	log.Fatal("Error al consumir mensajes:", err)
+	// }
 
-	// Escucha los mensajes
-	go func() {
-		for msg := range msgs {
-			fmt.Printf("Recibido un mensaje: %s\n", msg.Body)
-		}
-	}()
+	// // Escucha los mensajes
+	// go func() {
+	// 	for msg := range msgs {
+	// 		fmt.Printf("Recibido un mensaje: %s\n", msg.Body)
+	// 	}
+	// }()
 	router := mux.NewRouter()
-	//	s := r.PathPrefix("/api").Subrouter()
+	// s := r.PathPrefix("/api").Subrouter()
 	router.HandleFunc("/api/recharge", routes.RechargeHandler).Methods("POST")
 	router.HandleFunc("/api/recharges/{id}", routes.HistoryHandler).Methods("GET")
 	//
